b140f5ab70c1062cfe4632d8ff41198f81cb8f36
jh-alexdr <jtobar@byupathway.edu>
2025-03-31 16:44:46 -0500

    temples

---
+++ scripts/filtered-main.js
@@ -0,0 +1,5 @@
+// Copy of main.js
+document.getElementById("menu-toggle").addEventListener("click", () => {
+    const nav = document.getElementById("main-nav");
+    nav.classList.toggle("active");
+});
